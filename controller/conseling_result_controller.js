ab3a26543581d399cb6873133bccb74bdac79059
pandudewa <pdewapambudi@gmail.com>
2023-12-11 10:45:49 +0700

    mengedit insert counseling_result

--- controller/conseling_result_controller.js
+++ controller/conseling_result_controller.js
@@ -12,13 +12,16 @@ exports.addConselingResultTeacher = async (request, response) => {
     let result = {
         id_conseling: request.params.id,
         conseling_result: request.body.conseling_result,
-        // tipe_user: "teacher",
-        // counseling: request.body.Chat
     }
 
 
     counselingResultModel.create(result)
     .then(async (result) => {
+
+        await conselingModel.update({ isclosed: true }, {
+            where: { id: result.id_conseling }
+        });
+
         return response.json({
             message: "success",
             status: true,
@@ -30,38 +33,4 @@ exports.addConselingResultTeacher = async (request, response) => {
             message: error.message
         })
     })
-    // conselingModel.create(newConseling)
-    //     .then(async (result) => {
-
-    //         let id = result.id_conseling
-
-            // let idTeacher = result.id_teacher
-
-            // const tipeUserFromRequest = request.body.tipe_user;
-
-            // const isValidTipeUser = await onlineModel.exists({ value: tipeUserFromRequest });
-
-            // let conseling_result = {
-            //     id_conseling: id,
-                // id_user: idTeacher,
-                // conseling_result: request.body.conseling_result
-                // counseling: request.body.counseling
-            // };
-
-            // counselingResultModel.create(conseling_result)
-            //     .then(async (result) => {
-            //         return response.json({
-            //              message: 'success',
-            //              status: true,
-            //              data: result, 
-            //             })
-            //     })
-
-
-        // })
-        // .catch(error => {
-        //     return response.json({
-        //         message: error.message
-        //     })
-        // })
 }
